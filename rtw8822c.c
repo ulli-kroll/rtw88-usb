ba84ca2ede1173074d47a48c8b3a7a395a4cd188
Colin Ian King <colin.king@canonical.com>
2020-08-06 13:08:03 +0100

    rtw88: fix spelling mistake: "unsupport" -> "unsupported"
    There are some spelling mistakes in rtw_info messages. Fix these.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200806120803.60113-1-colin.king@canonical.com

--- rtw8822c.c
+++ rtw8822c.c
@@ -2023,7 +2023,7 @@ static int rtw8822c_set_antenna(struct rtw_dev *rtwdev,
 	case BB_PATH_AB:
 		break;
 	default:
-		rtw_info(rtwdev, "unsupport tx path 0x%x\n", antenna_tx);
+		rtw_info(rtwdev, "unsupported tx path 0x%x\n", antenna_tx);
 		return -EINVAL;
 	}
 
@@ -2033,7 +2033,7 @@ static int rtw8822c_set_antenna(struct rtw_dev *rtwdev,
 	case BB_PATH_AB:
 		break;
 	default:
-		rtw_info(rtwdev, "unsupport rx path 0x%x\n", antenna_rx);
+		rtw_info(rtwdev, "unsupported rx path 0x%x\n", antenna_rx);
 		return -EINVAL;
 	}
 
