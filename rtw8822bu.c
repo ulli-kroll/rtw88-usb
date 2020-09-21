22bbb9a96704cc6d24828ed72d0fbd6238d9453e
Hans Ulli Kroll <linux@ulli-kroll.de>
2020-09-21 14:57:55 +0200

    add rtw8822bu USB id's
    Signed-off-by: Hans Ulli Kroll <linux@ulli-kroll.de>

--- rtw8822bu.c
+++ rtw8822bu.c
@@ -6,6 +6,8 @@
 #include <linux/usb.h>
 #include "rtw8822bu.h"
 
+#define RTW_USB_VENDER_ID_EDIMAX	0x7392
+
 static const struct usb_device_id rtw_8822bu_id_table[] = {
 	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK,
 				       RTW_USB_PRODUCT_ID_REALTEK_8812B,
@@ -14,6 +16,24 @@ static const struct usb_device_id rtw_8822bu_id_table[] = {
 	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK,
 				       RTW_USB_PRODUCT_ID_REALTEK_8822B,
 				       0xff, 0xff, 0xff),
+	  .driver_info = (kernel_ulong_t)&(rtw8822b_hw_spec) },
+	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDER_ID_EDIMAX,
+					0xB822,
+					0xff, 0xff, 0xff),
+	  .driver_info = (kernel_ulong_t)&(rtw8822b_hw_spec) },
+	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDER_ID_EDIMAX,
+					0xC822,
+					0xff, 0xff, 0xff),
+	  .driver_info = (kernel_ulong_t)&(rtw8822b_hw_spec) },
+	{ USB_DEVICE(0x0b05, 0x184c),	/* ASUS AC53 Nano */
+	  .driver_info = (kernel_ulong_t)&(rtw8822b_hw_spec) },
+	{ USB_DEVICE(0x0b05, 0x1841),	/* ASUS AC55 B1 */
+	  .driver_info = (kernel_ulong_t)&(rtw8822b_hw_spec) },
+	{ USB_DEVICE(0x2001, 0x331c),	/* D-Link DWA-182 rev D1 */
+	  .driver_info = (kernel_ulong_t)&(rtw8822b_hw_spec) },
+	{ USB_DEVICE(0x13b1, 0x0043),	/* Linksys WUSB6400M */
+	  .driver_info = (kernel_ulong_t)&(rtw8822b_hw_spec) },
+	{ USB_DEVICE(0x2357, 0x012D),	/* TP-Link AC1300 T3U */
 	  .driver_info = (kernel_ulong_t)&(rtw8822b_hw_spec) },
 	{},
 };
