9aab21e673fd6aa6b1626bdfa073c5680cd2d344
Hans Ulli Kroll <linux@ulli-kroll.de>
2020-10-13 19:40:03 +0200

    Revert "add rtw8822cu USB id's"
    These USB iD's belong to the new development/chipset/rtl881cu
    branch
    This reverts commit d6b859eed430c24cb74301cafaa1eab25f103dab.

--- rtw8822cu.c
+++ rtw8822cu.c
@@ -15,48 +15,6 @@ static const struct usb_device_id rtw_8822cu_id_table[] = {
 					RTW_USB_PRODUCT_ID_REALTEK_8812C,
 					0xff, 0xff, 0xff),
 	  .driver_info = (kernel_ulong_t)&(rtw8822c_hw_spec) },
-	/* ULLI :
-	 * note these are supposed to be rtl8821cu devices
-	 * don't know, the source will *tell* us
-	 */
-	/*=== Realtek demoboard ===*/
-	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK,
-					0xb82b,
-					0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8822c_hw_spec) }, /* 8821CU */
-	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK,
-					0xb820,
-					0xff, 0xff, 0xff),
-	 .driver_info = (kernel_ulong_t)&(rtw8822c_hw_spec) }, /* 8821CU */
-	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK,
-					0xC821,
-					0xff, 0xff, 0xff),
-	 .driver_info = (kernel_ulong_t)&(rtw8822c_hw_spec) }, /* 8821CU */
-	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK,
-					0xC820,
-					0xff, 0xff, 0xff),
-	 .driver_info = (kernel_ulong_t)&(rtw8822c_hw_spec) }, /* 8821CU */
-	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK,
-					0xC82A,
-					0xff, 0xff, 0xff),
-	 .driver_info = (kernel_ulong_t)&(rtw8822c_hw_spec) }, /* 8821CU */
-	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK,
-					0xC82B,
-					0xff, 0xff, 0xff),
-	  .driver_info = (kernel_ulong_t)&(rtw8822c_hw_spec) }, /* 8821CU */
-	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK,
-					0xC811,
-					0xff, 0xff, 0xff),
-	 .driver_info = (kernel_ulong_t)&(rtw8822c_hw_spec) }, /* 8811CU */
-	{ USB_DEVICE_AND_INTERFACE_INFO(RTW_USB_VENDOR_ID_REALTEK,
-					0x8811,
-					0xff, 0xff, 0xff),
-	.driver_info = (kernel_ulong_t)&(rtw8822c_hw_spec) }, /* 8811CU */	
-	/*=== Customer ID ===*/
-	{ USB_DEVICE(0x0bda, 0x2006),
-	  .driver_info = (kernel_ulong_t)&(rtw8822c_hw_spec) }, /* Totolink */
-
-
 	{},
 };
 MODULE_DEVICE_TABLE(usb, rtw_8822cu_id_table);
