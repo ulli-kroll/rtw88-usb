1bf5d818953535dce3faa30456034d5fa7d32925
Hans Ulli Kroll <linux@ulli-kroll.de>
2020-10-15 20:05:11 +0200

    rtw8723du:fix module name
    Signed-off-by: Hans Ulli Kroll <linux@ulli-kroll.de>

--- rtw8723du.c
+++ rtw8723du.c
@@ -20,7 +20,7 @@ static const struct usb_device_id rtw_8723du_id_table[] = {
 MODULE_DEVICE_TABLE(usb, rtw_8723du_id_table);
 
 static struct usb_driver rtw_8723du_driver = {
-	.name = "rtw_8822bu",
+	.name = "rtw_8723du",
 	.id_table = rtw_8723du_id_table,
 	.probe = rtw_usb_probe,
 	.disconnect = rtw_usb_disconnect,
