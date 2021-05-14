c585495c2ded8dc5ea239c5740b89f22137b223c
Chin-Yen Lee <timlee@realtek.com>
2021-05-14 15:55:17 +0800

    rtw88: notify fw when driver in scan-period to avoid potential problem
    It is found that driver scan could be affected by dynamic mechanism
    of firmware, so we notify firmware to stop it in the scan period.
    Another, firmware will detect the background noise and report to
    driver for further use.
    
    Signed-off-by: Chin-Yen Lee <timlee@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20210514075517.14216-3-pkshih@realtek.com

--- main.h
+++ main.h
@@ -1741,6 +1741,8 @@ struct rtw_dm_info {
 	struct rtw_iqk_info iqk;
 	struct rtw_gapk_info gapk;
 	bool is_bt_iqk_timeout;
+
+	u8 scan_density;
 	s8 l2h_th_ini;
 	enum rtw_edcca_mode edcca_mode;
 };
@@ -1999,6 +2001,7 @@ struct rtw_dev {
 	struct rtw_wow_param wow;
 
 	bool need_rfk;
+	struct completion fw_scan_density;
 
 	/* hci related data, must be last */
 	u8 priv[] __aligned(sizeof(void *));
@@ -2104,6 +2107,7 @@ int rtw_sta_add(struct rtw_dev *rtwdev, struct ieee80211_sta *sta,
 void rtw_sta_remove(struct rtw_dev *rtwdev, struct ieee80211_sta *sta,
 		    bool fw_exist);
 void rtw_fw_recovery(struct rtw_dev *rtwdev);
+void rtw_core_fw_scan_notify(struct rtw_dev *rtwdev, bool start);
 int rtw_dump_fw(struct rtw_dev *rtwdev, const u32 ocp_src, u32 size,
 		const char *prefix_str);
 int rtw_dump_reg(struct rtw_dev *rtwdev, const u32 addr, const u32 size,
